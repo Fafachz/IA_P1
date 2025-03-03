c54899e6af5fe840abf2a70a24a91d2b2ae9ea1f
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 12:59:56 -0600

    Add files via upload

---
+++ 09_Listas.py
@@ -0,0 +1,13 @@
+#Colores
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"] #Declaramos una lista de colores
+#print(colores[3]) #Deacuerdo a la pregunta, en la posicion 3 esta el color AMARILLO
+#Rojo en la posicion 0 y Rosa en posicion 7
+
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2.
+num = ["tres","dos","cinco","cuatro","uno"] #Declaramos una lista en el orden pedido
+print(num[4]) #Muestra la lista segun el numero solicitado
