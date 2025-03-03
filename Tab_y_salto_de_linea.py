3317684b434a822e82e77bcc6b8dd1f74fc3367b
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 11:20:16 -0600

    Add files via upload

---
+++ Tab_y_salto_de_linea.py
@@ -0,0 +1,7 @@
+#Salto de Linea
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.")
+#Imprimimos las palabras con el salto de linea (\n)
+
+#Tabulacion
+print("\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.")
+#Imprimimos, además del salto, con una tabulación(\t)
