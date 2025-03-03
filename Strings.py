5b4d58b91caaac26c579d9486e3ded4d80e0acc2
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 10:19:42 -0600

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,7 @@
+msj1 = "String con comillas dobles" #Declaramos un string con comillas dobles
+msj2 = 'String con comillas simples' #Declaramos un stirng con comillas simples
+
+print(msj1); print(msj2) #Imprimimos los dos mensajes
+
+Mensaje = '"print()" se utiliza para imprimir valores en la consola.' #Almacenamos una frase que contenga comillas
+print(Mensaje) #Imprimimos la frase
