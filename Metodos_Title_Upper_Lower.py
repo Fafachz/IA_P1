c8c8b7b2e7350affa0f7e5631781c8553b807f83
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 12:46:50 -0600

    Delete Metodos_Title_Upper_Lower.py

--- Metodos_Title_Upper_Lower.py
+++
@@ -1,6 +0,0 @@
-nombre = "farid ariel orozco sánchez" #Declaramos un string completamente en minusculas
-print(nombre.title()) #Imprimimos en formato Titulo
-
-frase = "Esta eS una Frase pAra Ser formateada" #Declaramos otro string con minusculas y mayusculas
-print(frase.upper()) #Imprimimos string en mayusculas
-print(frase.lower()) #Imprimimos string en minusculas
