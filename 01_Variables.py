d4a58eba2e663100973ad63d7f783bf9951918dc
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 12:45:43 -0600

    Add files via upload

---
+++ 01_Variables.py
@@ -0,0 +1,9 @@
+#Saludo
+VariableSaludo = "Hola mi beautiful world" #Almacenamos el string en la variable
+print(VariableSaludo) #Imprimimos la variable con el valor contenido
+
+#Suma
+x = 5 #Declaramos la primer variable 
+y = 8 #Declaramos la segunda variable 
+z = x+y #Sumamos las variables previamente creadas y lo almacenamos en la variable z
+print(z) #Imprimimos el resultado de la suma
---
+++ 08_Float_y_round.py
@@ -0,0 +1,2 @@
+num = 17.567383292929200234 #Declaramos la variable como tipo float
+print(round(num,5)) #Utilizamos round para decidir los decimales que se van a mostrar
