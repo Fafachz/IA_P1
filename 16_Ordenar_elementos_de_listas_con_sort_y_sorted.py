d9e66702b919f688960589d8fa7b87d8b48a6d60
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 19:52:38 -0600

    Add files via upload

---
+++ 16_Ordenar_elementos_de_listas_con_sort_y_sorted.py
@@ -0,0 +1,9 @@
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+
+#Creamos y mostramos lista
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores)
+
+#Reordenamos y mostramos
+colores.sort(reverse=True)
+print(colores)
