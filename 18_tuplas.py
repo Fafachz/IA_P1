7dd5b8f9690d8e434c40c26d0d84e678093273d5
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 20:28:31 -0600

    Add files via upload

---
+++ 18_tuplas.py
@@ -0,0 +1,9 @@
+#Imprime la segunda posición de esta tupla
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(colores[1])
+
+#Utiliza los símbolos de suma y resta para obtener el resultado 25 
+# a partir de los elementos de la siguiente tupla en una variable llamada operacion
+num = (10, 1, 5, 11)
+operacion = num[0] - num[1] + num[2] + num[3]
+print(operacion)
