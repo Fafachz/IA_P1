4603e56af1bcccfb8a43ca8902ce7157ceb8d7cd
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 20:43:38 -0600

    Add files via upload

---
+++ 19_tupla_a_lista_y_viceversa.py
@@ -0,0 +1,8 @@
+#Convierte la siguiente lista en una tupla y 
+#asegúrate que se haya convertido en tupla correctamente imprimiendo en la consola el tipo de elemento que es.
+
+#creamos lista
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+color_tupla = tuple(colores)#Convertimos lista en tupla
+print(color_tupla)#Imprimimos tupla
+print(type(color_tupla))#Revisamos que clase es
