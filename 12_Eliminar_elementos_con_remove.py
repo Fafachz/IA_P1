670cea103107c3f62c650ca09d15d8886cb9f0d3
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 18:58:03 -0600

    Add files via upload

---
+++ 12_Eliminar_elementos_con_remove.py
@@ -0,0 +1,6 @@
+#Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'. Solo puedes eliminarlos utilizando el método remove().
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #Crea lista de colores
+colores.remove('amarillo') #Elimina elemendo de la lista, en este caso color amarillo
+colores.remove('rojo') #Elimina elemento de la lista, en este otro caso color rojo
+print(colores) #Imprime la lista completa en la terminal
