3df1cae81a0cdc8477fb8f286d5a6d93d49cab47
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 20:05:45 -0600

    Add files via upload

---
+++ 17_Contar_elementos_con_len.py
@@ -0,0 +1,10 @@
+#En este capitulo no hay ejercicio
+
+#Ejemplo 1
+colores = ['rojo', 'azul', 'verde', 'amarillo']
+print(len(colores))
+
+#Ejemplo 2
+colores2 = ['rojo', 'azul', 'verde', 'amarillo']
+longitud_colores = len(colores2)
+print(longitud_colores)
