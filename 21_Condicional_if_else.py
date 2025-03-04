36e2b1868821f326f20375db51875ea6f8a42a14
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 20:57:32 -0600

    Add files via upload

---
+++ 21_Condicional_if_else.py
@@ -0,0 +1,15 @@
+#Corrige el siguiente condicional if else.
+
+#color = rojo
+
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+#Correccion:
+color = 'rojo'
+if color == 'rojo':
+    print('El color es rojo')
+else:
+    print('El color no es rojo')
