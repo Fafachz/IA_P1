a7c7a3d8a6bb261d3e1bc44f33f63d7a1b7b3a5d
Fafachz <fafa_41m4@hotmail.com>
2025-03-04 11:09:11 -0600

    Add files via upload

---
+++ 24_while.py
@@ -0,0 +1,21 @@
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5.
+print('Bucle 1')
+x = 0
+while x <= 15:
+	print(x)
+	x += 5
+
+#Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20.
+print('Bucle 2')	
+x = 0 
+while x >= -100:
+	print(x)
+	x -= 20
+
+#Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de 1 y que muestre en 
+#cada ejecución esta frase con el valor de ejecución correspondiente: 'El valor del bucle es 10'... 
+print('Bucle 3')	
+x = 10 
+while x >= 0:
+	print('El calor del bucle es: ',x)
+	x -= 1
