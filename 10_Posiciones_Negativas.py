1ea6b1425411eb677b55faa1c70ca249f2607b91
Fafachz <fafa_41m4@hotmail.com>
2025-03-03 19:28:05 -0600

    Add files via upload

---
+++ 10_Posiciones_Negativas.py
@@ -0,0 +1,11 @@
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista. 
+# Los colores a los que tienes que acceder son 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#Creamos la lista
+
+print(colores[-1]) #Imprimos el ultimo color/elemento de la lista, color naranja
+print(colores[3]) #Imprimos color amarillo
+print(colores[-5]) #Imprimos color lila
+print(colores[-2]) #Imprimos el penultimo color/elemento de la lista, color blanco
+print(colores[0]) #Imprimimos el color rojo
---
+++ 14_Insertal_elementos_con_append.py
@@ -0,0 +1,13 @@
+#Añade los colores 'fuxia' y 'celeste' a la lista
+
+#Creamos lista:
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+#Imprimimos antes de agregar colores
+print(colores)
+
+#Utilizamos append() para agregar colores:
+colores.append('fuxia'), colores.append('celeste')
+
+#Imprimimos lista:
+print(colores)
