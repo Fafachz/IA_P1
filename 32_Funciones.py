3e076fb7e23d36c114fa1caf50cb4d91c0a3fb05
Fafachz <fafa_41m4@hotmail.com>
2025-03-04 23:43:21 -0600

    Add files via upload

---
+++ 32_Funciones.py
@@ -0,0 +1,11 @@
+#Crea una función que realice una suma. Para ello, tendrás que añadir dos argumentos(numero1 y numero2).
+#En su interior, especificarás un print() que muestre el resultado de la suma. Deberás hacer tres llamadas 
+# que como resultado de la suma den los valores 30, 50 y 57000. 
+#Los números que introduzcas en la llamada pueden ser los que quieras siempre que coincidan los resultados en el print().
+
+def sumas(num1, num2): #Creamos la funcion de suma con 2 valores
+    print(num1 + num2) #Imprimimos la operacion entre 2 numeros
+
+sumas(15, 15) #Llamamos la funcion suma para agregar 2 numeros que den 30
+sumas(35, 15) #Llamamos la funcion suma para agregar 2 numeros que den 50
+sumas(34000, 23000) #Llamamos la funcion suma para agregar 2 numeros que den 57000
