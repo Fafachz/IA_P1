92b1a50340d7ab5208a66e297de852ce7d17caf1
Fafachz <fafa_41m4@hotmail.com>
2025-03-04 23:01:16 -0600

    Add files via upload

---
+++ 29_diccionarios.py
@@ -0,0 +1,17 @@
+#Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio con presentación en un print().
+#El resultado será esto en la consola:
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+
+print('El modelo es: ' + teclado2['Modelo'] + ', el precio es: ' + teclado2['Precio'] + '.')
