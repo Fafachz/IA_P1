397761996003b33ca716f5ff940e1e6dd6a4fb00
Fafachz <fafa_41m4@hotmail.com>
2025-03-04 22:51:20 -0600

    Add files via upload

---
+++ 26_For.py
@@ -0,0 +1,5 @@
+#Crea un bucle for que itere la siguiente tupla y muestre una frase como esta en cada iteración: 'El color es: ' + color + '.'.
+
+colors = ('Rojo','Azul','Verde','Amarillo') #Creamos tupla con colores
+for x in colors: #Creamos bucle for que itera los elementos de la tupla
+    print('El color es: ' + x + '.')#Imprimimos la tupla
